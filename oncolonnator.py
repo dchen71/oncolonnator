9d8c8c6769a0d1de8b400abb23c7070abebdf375
Daniel Chen <dchen12812@gmail.com>
2020-06-29 12:30:35 -0700

    docstrings

--- oncolonnator.py
+++ oncolonnator.py
@@ -12,7 +12,14 @@ import os
 ## https://pyvcf.readthedocs.io/en/latest/INTRO.html
 ## https://samtools.github.io/hts-specs/VCFv4.2.pdf
 
-def annotate_vcfs(input_vcf, output_file):
+def annotate_vcfs(input_vcf = None, output_file = 'output/parsed.csv'):
+    """
+    Parses and annotates a given vcf file using the ExAC variant database
+
+    Keyword arguments: 
+    input_vcf -- The path to the VCF file to annotate (default None)
+    output_file -- The directory and file name to output a csv to (default output/parsed.csv)
+    """
     try:
       vcf_reader = vcf.Reader(open(input_vcf, 'r'))
     except:
