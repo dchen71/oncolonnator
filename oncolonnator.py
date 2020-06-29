278ee741684dab23a5d33702cd8eaab8d5c87e14
Daniel Chen <dchen12812@gmail.com>
2020-06-29 12:27:19 -0700

    basic trycatch reader

--- oncolonnator.py
+++ oncolonnator.py
@@ -6,16 +6,20 @@ import pandas as pd
 import argparse
 import requests
 import json
+import os
 
 # Function to do basic vcf manipulations
 ## https://pyvcf.readthedocs.io/en/latest/INTRO.html
 ## https://samtools.github.io/hts-specs/VCFv4.2.pdf
 
 def annotate_vcfs(input_vcf, output_file):
-    print("hello world!")
+    try:
+      vcf_reader = vcf.Reader(open(input_vcf, 'r'))
+    except:
+      print("Error: Could not open input file")
 
 # GET request API calls to ExAC
 ## https://www.geeksforgeeks.org/get-post-requests-using-python/
 
 if __name__ == "__main__":
-    annotate_vcfs("input/")
+    annotate_vcfs("input/example.vcf")
