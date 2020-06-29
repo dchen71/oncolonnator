626741e8b5f94b5a991b361413b3b8214593bdd1
Daniel Chen <dchen12812@gmail.com>
2020-06-29 12:19:45 -0700

    package loading

---
+++ oncolonnator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Module imports
+import vcf
+import pandas as pd
+import argparse
+import requests
+import json
+
+# Function to do basic vcf manipulations
+## https://pyvcf.readthedocs.io/en/latest/INTRO.html
+## https://samtools.github.io/hts-specs/VCFv4.2.pdf
+
+def annotate_vcfs(input_vcf, output_file):
+    print("hello world!")
+
+# GET request API calls to ExAC
+## https://www.geeksforgeeks.org/get-post-requests-using-python/
+
+if __name__ == "__main__":
+    annotate_vcfs("input/")
