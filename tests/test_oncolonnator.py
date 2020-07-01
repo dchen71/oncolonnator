9b8fd330982cbf71c44c68d9473a32976f9a2459
Daniel Chen <dchen12812@gmail.com>
2020-07-01 15:43:53 -0700

    init logic

--- tests/test_oncolonnator.py
+++ tests/test_oncolonnator.py
@@ -36,8 +36,18 @@ class TestOncolonnator:
         snp1 = get_variant_annotation(chromosome = 14, position = 21853913, ref = 'T', alt = 'C')
         assert(snp1 == [4.6048996131884326e-05,'non_coding_transcript_exon_variant', ['ENSG00000100888'], ['ENST00000430710', 'ENST00000399982', 'ENST00000557364', 'ENST00000557727']])
 
-    def test_annotate_vcfs(self):
+    def test_annotate_vcfs(self, tmp_path):
         """
         Test end to end annotation function
         """
-        pass
+        # Create test vcf file
+        print(tmp_path)
+        tmp_dir = tmp_path.mkdir()
+        file1 = tmp_dir / "test.csv"
+        file1.write("1	931393	.	G	T	2.17938e-13	.	AB=0;ABP=0;AC=0;AF=0;AN=6;AO=95;CIGAR=1X;DP=4124;DPB=4124;DPRA=0.999031;EPP=9.61615;EPPR=316.776;GTI=0;LEN=1;MEANALT=1;MQM=59.7579;MQMR=65.2274;NS=2;NUMALT=1;ODDS=591.29;PAIRED=0.989474;PAIREDR=0.966741;PAO=0;PQA=0;PQR=0;PRO=0;QA=3774;QR=160284;RO=4029;RPL=51;RPP=4.13032;RPPR=101.278;RPR=44;RUN=1;SAF=40;SAP=8.15326;SAR=55;SRF=1663;SRP=269.369;SRR=2366;TYPE=snp	GT:GQ:DP:DPR:RO:QR:AO:QA	0/0/0:132.995:2063:2063,0:2063:82063:0:0	0/0/0:132.995:2061:2061,95:1966:78221:95:3774")
+        file.close()
+
+        annotate_vcfs(file1, tmp_path.mkdir("test").join("output.csv"))
+
+        assert(os.path.exists(tmp_path))
+
